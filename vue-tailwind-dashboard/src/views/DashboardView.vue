30be54c5a074cb8ea2621ce6d1cc8a1a5c81f42c
Ronny Charlton Cabrera Sinarahua <ronny.cabrera.sinarahua@gmail.com>
2025-06-30 11:03:05 -0500

    fix templates

--- vue-tailwind-dashboard/src/views/DashboardView.vue
+++ vue-tailwind-dashboard/src/views/DashboardView.vue
@@ -8,37 +8,38 @@
         <StatsCard
           title="Ingresos Totales"
           :value="store.totalIncome"
-        valueCurrencyCode="PEN" <!-- Assuming store totals are effectively PEN -->
-        icon="fa-solid fa-arrow-up"
-        icon-bg-color="bg-green-100"
-        icon-text-color="text-green-600"
-        value-text-color="text-green-600"
-      />
-      <StatsCard
-        title="Gastos Totales"
-        :value="store.totalExpense"
-        valueCurrencyCode="PEN" <!-- Assuming store totals are effectively PEN -->
-        icon="fa-solid fa-arrow-down"
-        icon-bg-color="bg-red-100"
-        icon-text-color="text-red-600"
-        value-text-color="text-red-600"
-      />
-      <StatsCard
-        title="Balance Actual"
-        :value="store.balance"
-        valueCurrencyCode="PEN" <!-- Assuming store totals are effectively PEN -->
-        icon="fa-solid fa-wallet"
-        icon-bg-color="bg-blue-100"
-        icon-text-color="text-blue-600"
-        :value-text-color="store.balance < 0 ? 'text-red-600' : 'text-green-600'"
-      />
-    </section>
+          valueCurrencyCode="PEN"
+          icon="fa-solid fa-arrow-up"
+          icon-bg-color="bg-green-100"
+          icon-text-color="text-green-600"
+          value-text-color="text-green-600"
+        />
+        <StatsCard
+          title="Gastos Totales"
+          :value="store.totalExpense"
+          valueCurrencyCode="PEN"
+          icon="fa-solid fa-arrow-down"
+          icon-bg-color="bg-red-100"
+          icon-text-color="text-red-600"
+          value-text-color="text-red-600"
+        />
+        <StatsCard
+          title="Balance Actual"
+          :value="store.balance"
+          valueCurrencyCode="PEN"
+          icon="fa-solid fa-wallet"
+          icon-bg-color="bg-blue-100"
+          icon-text-color="text-blue-600"
+          :value-text-color="store.balance < 0 ? 'text-red-600' : 'text-green-600'"
+        />
+      </section>
 
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
-      <div class="lg:col-span-2">
-        <TransactionList />
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div class="lg:col-span-2">
+          <TransactionList />
+        </div>
+        <CategoryList />
       </div>
-      <CategoryList />
     </div>
     <!-- Loading state -->
     <div v-else class="text-center py-10">
